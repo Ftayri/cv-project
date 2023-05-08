3b047ae7983bdaf1d193c01486dffeeb859daa0b
Iyed <75183184+Ftayri@users.noreply.github.com>
2023-05-08 21:07:13 +0100

    Adjusted drawer margins

--- lib/menu/drawer.widget.dart
+++ lib/menu/drawer.widget.dart
@@ -24,13 +24,14 @@ class _MyDrawerState extends State<MyDrawer> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Padding(
-            padding: const EdgeInsets.all(60.0),
+            padding: const EdgeInsets.only(top: 60, right: 60, left: 60, bottom: 25),
             child: CircleAvatar(
               backgroundImage: AssetImage('assets/iyed.jpg'),
               backgroundColor: theme.tertiary,
               radius: 50,
             ),
           ),
+          Text("Iyed Zairi"),
 
           Expanded(
             child: ListView.builder(
@@ -57,7 +58,7 @@ class _MyDrawerState extends State<MyDrawer> {
         ZoomDrawer.of(context)?.toggle();
       },
       child: Container(
-        margin: EdgeInsets.only(left: 20, bottom: 12),
+        margin: EdgeInsets.only(left: 20, bottom: 25),
         child: Row(
           children: [
             icon,
