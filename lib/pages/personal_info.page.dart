88c757ae29ef35107d40e1375c0c1e893241dd26
Iyed <75183184+Ftayri@users.noreply.github.com>
2023-05-08 22:41:00 +0100

    Finished personal info page

--- lib/pages/personal_info.page.dart
+++ lib/pages/personal_info.page.dart
@@ -1,3 +1,4 @@
+import 'package:cv/config/my_theme.dart';
 import 'package:cv/menu/drawer.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -5,36 +6,66 @@ import 'package:url_launcher/url_launcher.dart';
 class PersonalInfoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
     return Scaffold(
       appBar: AppBar(
         title: Text('Informations Personelles'),
         leading: DrawerWidget(),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            ContactRow(
-              icon: Icons.person,
-              title: 'Nom & Prénom:',
-              value: 'Iyed Zairi',
-            ),
-            SizedBox(height: 16.0),
-            ContactRow(
-              icon: Icons.email,
-              title: 'Email:',
-              value: 'iyed.zairi@gmail.com',
-            ),
-            SizedBox(height: 16.0),
-            ContactRow(
-              icon: Icons.phone,
-              title: 'Numéro de Téléphone',
-              value: '25 25 31 25',
-            ),
-            SizedBox(height: 16.0),
-            SocialMediaIcons(),
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('assets/iyed.jpg'),
+                ),
+              ),
+              Text(
+                'Etudient en Génie Logiciel et Informatique Décisionnel',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 12),
+              InfoCard(
+                  icon: Icons.person,
+                  title: "Nom & Prénom",
+                  value: "Iyed Zairi"),
+              SizedBox(height: 12),
+              InfoCard(
+                  icon: Icons.phone,
+                  title: "Numéro de Téléphone",
+                  value: "+216 25 25 31 25"),
+              SizedBox(height: 12),
+              InfoCard(
+                  icon: Icons.email,
+                  title: "E-mail",
+                  value: "iyed.zairi@gmail.com"),
+              SizedBox(height: 12),
+              InfoCard(
+                  icon: Icons.cake,
+                  title: "Date & Lieu de Naissance",
+                  value: "05/12/1999 à Sfax"),
+              SizedBox(height: 12),
+              InfoCard(
+                  icon: Icons.place,
+                  title: "Adresse",
+                  value: "Route de Tunis KM10, Cité Ons, Sfax"),
+              InfoCard(
+                  icon: Icons.description,
+                  title: "Profil",
+                  value: "Étudiant en génie informatique. Compétent dans plusieurs domaines, y compris la programmation (C/C++/Python/Java/JavaScript), la résolution de problèmes et la mise" +
+                      "en réseau. Apprend rapidement, travaille dur, innovant, responsable, complaisant et" +
+                      " peut facilement s'adapter aux nouveaux défis"),
+              SizedBox(height: 12),
+
+              // Add more rows with cards for other personal info here
+            ],
+          ),
         ),
       ),
     );
@@ -45,7 +76,6 @@ class SocialMediaIcons extends StatelessWidget {
   final Uri _linkedinUri = Uri.parse('https://linkedin.com');
 
   SocialMediaIcons({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -70,35 +100,26 @@ class SocialMediaIcons extends StatelessWidget {
   }
 }
 
-class ContactRow extends StatelessWidget {
+class InfoCard extends StatelessWidget {
   final IconData icon;
   final String title;
   final String value;
 
-  ContactRow({required this.icon, required this.title, required this.value});
+  InfoCard({required this.icon, required this.title, required this.value});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Icon(icon, size: 24.0, color: Theme.of(context).primaryColor,),
-        SizedBox(width: 16.0),
-        Expanded(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-              Text(title, style: TextStyle(fontSize: 16.0)),
-          SizedBox(height: 8.0),
-          Text(
-            value,
-            style: TextStyle(fontSize: 20.0),
-          ),
-      ],
-    ),)
-    ,
-    ]
-    ,
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    return Card(
+      elevation: 3,
+      child: ListTile(
+        leading: Icon(icon,
+            color: isDark
+                ? MyTheme.myDarkTheme.primary
+                : MyTheme.myLightTheme.primary),
+        title: Text(title),
+        subtitle: Text(value),
+      ),
     );
   }
 }
