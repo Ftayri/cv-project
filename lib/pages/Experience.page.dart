febd404c9470fca3d1cb50ed3b6812e923be14a5
Iyed <75183184+Ftayri@users.noreply.github.com>
2023-05-09 14:55:55 +0100

    Initialized Map development

--- lib/pages/Experience.page.dart
+++ lib/pages/Experience.page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:latlong2/latlong.dart';
 
 import '../menu/drawer.widget.dart';
 
@@ -11,31 +13,64 @@ class ExperienceScreen extends StatelessWidget {
         title: Text('Expériences Professionnelles'),
         leading: DrawerWidget(),
       ),
-      body: ListView(
+      body: Column(
         children: [
-          ExperienceCard(
-            company: 'CLINISYS',
-            role: 'Stage d"initiation',
-            duration: 'Juin 2022 - Aout 2022',
-            description: 'Développement d"une interface interactive et esthétique qui offre une expérience utilisateur optimale sur différents navigateurs et appareils.',
-            color: Colors.blue,
-            icon: FontAwesomeIcons.building,
+          Expanded(
+            child: FlutterMap(
+              options: MapOptions(
+                center: LatLng(51.509364, -0.128928),
+                zoom: 9.2,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                ),
+              ],
+              nonRotatedChildren: [
+                RichAttributionWidget(
+                  attributions: [
+                    TextSourceAttribution(
+                      'OpenStreetMap contributors',
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-          ExperienceCard(
-            company: 'Platana',
-            role: 'Stage d"été',
-            duration: 'Juin 2021 - Aout 2021',
-            description:'Contribuer à Karrio, une plateforme d’expédition multi transporteurs open source extensible, en abstractant eten intégrant les services Webde Chronopost.',
-            color: Colors.green,
-            icon: FontAwesomeIcons.laptopCode,
-          ),
-          ExperienceCard(
-            company: '123 Corporation',
-            role: 'Stage d"été',
-            duration: 'Juin 2020 - Aout 2020',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            color: Colors.orange,
-            icon: FontAwesomeIcons.code,
+          Expanded(
+            child: ListView(
+              children: [
+                ExperienceCard(
+                  company: 'CLINISYS',
+                  role: 'Stage d"initiation',
+                  duration: 'Juin 2022 - Aout 2022',
+                  description:
+                  'Développement d"une interface interactive et esthétique qui offre une expérience utilisateur optimale sur différents navigateurs et appareils.',
+                  color: Colors.blue,
+                  icon: Icons.work,
+                ),
+                ExperienceCard(
+                  company: 'Platana',
+                  role: 'Stage d"été',
+                  duration: 'Juin 2021 - Aout 2021',
+                  description:
+                  'Contribuer à Karrio, une plateforme d’expédition multi transporteurs open source extensible, en abstractant eten intégrant les services Webde Chronopost.',
+                  color: Colors.green,
+                  icon: Icons.laptop,
+                ),
+                ExperienceCard(
+                  company: '123 Corporation',
+                  role: 'Stage d"été',
+                  duration: 'Juin 2020 - Aout 2020',
+                  description:
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                  color: Colors.orange,
+                  icon: Icons.code,
+                ),
+              ],
+            ),
           ),
         ],
       ),
